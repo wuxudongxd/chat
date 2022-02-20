32a110e36421497c180443a5f3f99d90615db7b7
wuxudongxd <wuxudongxd@gmail.com>
2022-02-20 17:41:31 +0800

    ✨ feat: chat gateway

--- packages/chat-server/src/app.module.ts
+++ packages/chat-server/src/app.module.ts
@@ -5,8 +5,17 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
 import { GroupModule } from './modules/group/group.module';
 import { FriendModule } from './modules/friend/friend.module';
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, PassportModule, UserModule, GroupModule, FriendModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    PassportModule,
+    UserModule,
+    GroupModule,
+    FriendModule,
+    ChatModule,
+  ],
 })
 export class AppModule {}
