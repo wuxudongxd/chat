27db3c4cdadcf100b3fb9755993b014a0248fb31
wuxudongxd <wuxudongxd@gmail.com>
2022-02-23 16:44:08 +0800

    🐞 fix: 新增 "no-undef": "off" rule
    https://typescript-eslint.io/docs/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors

--- packages/chat-client/.eslintrc.js
+++ packages/chat-client/.eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   extends: ["alloy", "alloy/react", "alloy/typescript"],
   env: {
     browser: true,
+    node: true,
   },
   globals: {},
-  rules: {},
+  rules: {
+    "no-param-reassign": "off",
+    // https://typescript-eslint.io/docs/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+    "no-undef": "off",
+  },
 };
