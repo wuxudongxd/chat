5fd7efbc2bfd23382ec8eec040f92e0109e27747
wuxudongxd <wuxudongxd@gmail.com>
2022-02-24 14:37:49 +0800

    🦄 refactor: extract jwt ( from fromHeader to fromAuthHeaderAsBearerToken)

--- packages/chat-server/src/modules/passport/jwt.strategy.ts
+++ packages/chat-server/src/modules/passport/jwt.strategy.ts
@@ -7,7 +7,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromHeader('token'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
