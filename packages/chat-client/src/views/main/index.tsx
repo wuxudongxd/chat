8825df929b84cd2738c416057d26854bdb1a69d2
wuxudongxd <wuxudongxd@gmail.com>
2022-02-26 23:23:18 +0800

    🦄 refactor: 调整文件逻辑

--- packages/chat-client/src/views/main/index.tsx
+++ packages/chat-client/src/views/main/index.tsx
@@ -7,8 +7,8 @@ const Main = () => {
     <div className="bg-[url('~/assets/bg-1.jpg')] bg-no-repeat bg-cover w-screen h-screen flex justify-center items-center">
       <div className="w-4/5 h-4/5 backdrop-blur-[50px] shadow-2xl rounded overflow-hidden flex">
         <Sidebar />
-        <Chat />
         <Room />
+        <Chat />
       </div>
     </div>
   );
--- packages/chat-client/src/views/main/room.tsx
+++ packages/chat-client/src/views/main/room.tsx
@@ -1,50 +1,73 @@
-import { Button } from "antd";
-import { useEffect, useState } from "react";
-import { useSocketIo } from "~/context/socket-io";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import { Dropdown, Menu, Select } from "antd";
 
 const Room = () => {
-  const socket = useSocketIo();
-  const [input, setInput] = useState("");
-  const [content, setContent] = useState<string[]>([]);
-
-  const handleEmit = (e: any) => {
-    e.preventDefault();
-    socket.volatile.emit("message", input);
-    setContent((prev) => [...prev, input]);
-    setInput("");
-  };
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      socket.on("message", (msg: string) => {
-        console.log("socket id: ", socket.id);
-        console.log("received:", msg);
-        setContent((prev) => [...prev, msg]);
-      });
-    });
-  }, []);
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          3rd menu item
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 
   return (
-    <div className="flex-1 flex flex-col">
-      <div className="h-14 w-full flex justify-center items-center bg-black/50">
-        <span className="text-lg text-gray-300">默认聊天室</span>
-      </div>
-      <div className="flex-1">
-        {content.map((item: string, index: number) => {
-          return <div key={index}>{item}</div>;
-        })}
-      </div>
-      <div className="w-full h-8 flex justify-between items-center">
-        <input
-          className="w-full h-full px-4 outline-0"
-          type="text"
-          placeholder="输入消息"
-          value={input}
-          onInput={(e) => setInput((e.target as HTMLInputElement).value)}
+    <div className="w-56 bg-black/20 flex flex-col">
+      <div className="h-14 p-3 flex items-center">
+        <Select
+          showSearch
+          placeholder="搜索聊天组"
+          className="w-full rounded-l"
         />
-        <Button type="primary" className="h-full" onClick={handleEmit}>
-          发送
-        </Button>
+        <Dropdown overlay={menu} placement="bottomCenter">
+          <div className="bg-white w-10 h-full rounded-r flex justify-center items-center">
+            <PlusCircleOutlined
+              className="text-2xl"
+              style={{ color: "#747d8c" }}
+            />
+          </div>
+        </Dropdown>
+      </div>
+      <div className="flex-1 overflow-auto">
+        {[...new Array(5)].map(() => {
+          return (
+            <div className="h-16 bg-black/25 flex items-center cursor-pointer">
+              <img
+                className="w-8 h-8 ml-4 rounded-full"
+                src="https://gravatar.com/avatar/placeholder?s=200"
+                alt="avatar"
+              />
+              <div className="ml-4 flex-1 flex flex-col justify-center text-gray-300">
+                <div className="text-base">name</div>
+                <div className="text-gray-400">message</div>
+              </div>
+            </div>
+          );
+        })}
+        <div className="h-16 bg-black/40 ">chat</div>
       </div>
     </div>
   );
