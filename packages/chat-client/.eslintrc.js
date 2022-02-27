9cf38ebd3c8916f35ae9e29a45c41e3a010fcb2e
wuxudongxd <wuxudongxd@gmail.com>
2022-02-27 17:48:07 +0800

    🐞 fix: update react hook eslint

--- packages/chat-client/.eslintrc.js
+++ packages/chat-client/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   extends: ["alloy", "alloy/react", "alloy/typescript"],
+  plugins: ["react-hooks"],
   env: {
     browser: true,
     node: true,
   },
   globals: {},
   rules: {
+    "react-hooks/rules-of-hooks": "error", // 检查 Hook 的规则
+    "react-hooks/exhaustive-deps": "warn", // 检查 effect 的依赖
     "no-param-reassign": "off",
     // https://typescript-eslint.io/docs/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
     "no-undef": "off",
