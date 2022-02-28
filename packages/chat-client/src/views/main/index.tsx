21c13ac0f482ee65f27db53435e52b4b42ed4d62
wuxudongxd <wuxudongxd@gmail.com>
2022-03-01 01:12:12 +0800

    🌈 style: 调整圆角样式

--- packages/chat-client/src/views/main/index.tsx
+++ packages/chat-client/src/views/main/index.tsx
@@ -5,7 +5,7 @@ import Sidebar from "./sidebar";
 const Main = () => {
   return (
     <div className="bg-[url('~/assets/bg-1.jpg')] bg-no-repeat bg-cover w-screen h-screen flex justify-center items-center">
-      <div className="w-4/5 h-4/5 backdrop-blur-[50px] shadow-2xl rounded overflow-hidden flex">
+      <div className="w-4/5 h-4/5 backdrop-blur-[50px] shadow-2xl rounded-md overflow-hidden flex">
         <Sidebar />
         <Room />
         <Chat />
